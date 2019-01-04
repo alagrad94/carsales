ca9b882b649e50b079763c56e641f41538699a81
Russell Reiter <russell.reiter@gmail.com>
2019-01-04 11:58:42 -0600

    Exercise complete.  Challenge not complete

---
+++ main.js
@@ -0,0 +1,75 @@
+let outputElement = document.getElementById("output");
+
+// allCars.forEach(car => {
+
+//     for (const value of Object.values(car)) {
+//         outputElement.innerHTML += `<div>${value}</div>`
+//       }
+// });
+
+// const firstCar = allCars[0]
+
+// // Now iterate its keys
+// outputElement.innerHTML += "<h1>Properties</h1>"
+// for (const key of Object.keys(allCars[0])) {
+//   outputElement.innerHTML += `<div>${key}</div>`
+// }
+
+// // Iterate the array of cars. Individual objects stored in `car`.
+// outputElement.innerHTML += "<h1>Car List</h1>"
+// allCars.forEach(car => {
+
+//     // Iterate all of the values of the current car
+//     for (const value of Object.values(car)) {
+//       outputElement.innerHTML += `<div>${value}</div>`
+//     }
+// })
+
+// outputElement.innerHTML += "<h1>Car List</h1>"
+
+// allCars.forEach(car => {
+//     outputElement.innerHTML += "<hr/>"
+
+//     for (const entry of Object.entries(car)) {
+//         console.log(entry)
+//         outputElement.innerHTML += `<div>${entry[0]}: ${entry[1]}</div>`
+//     }
+// })
+
+outputElement.innerHTML += "<h1>Weekly Sales Report</h1>"
+
+
+salesByWeek.forEach(sale => {
+
+    outputElement.innerHTML += "<hr/>"
+
+    outputElement.innerHTML += `<h3>${sale.sales_agent.first_name} ${sale.sales_agent.last_name}</h3>`;
+    for (const entry of Object.entries(sale.vehicle)) {
+        console.log(entry)
+        outputElement.innerHTML += `<div>${entry[0]}: ${entry[1]}</div>`
+    }
+
+    outputElement.innerHTML += `Gross Profit: $${sale.gross_profit}`
+})
+
+// let searchInput = document.getElementById("searchInput")
+
+// searchInput.addEventListener("keyup", event => {
+
+//   if (event.charCode === 13) {
+
+//     const searchTerm = event.target.value
+
+//     console.log(searchTerm)
+
+//     salesByWeek.forEach(sale => {
+//         console.log(sale)
+
+//     });
+//   }
+// })
+    // "sales_agent": {
+    // "mobile": "1-584-162-7444",
+    // "last_name": "Larkin",
+    // "first_name": "Tiara",
+    // "email": "ready@gmail.com"
